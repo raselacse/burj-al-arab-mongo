0f6c6af6f2e45884c0aa1936a726e55a2b3c200f
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-03-26 17:00:51 +0600

    Verify jwt token in the server end using firebase admin

--- burj-al-arab-server/index.js
+++ burj-al-arab-server/index.js
@@ -11,6 +11,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const cors = require('cors');
 app.use(cors());
 
+const admin = require('firebase-admin');
+var serviceAccount = require("../burj-al-arab-after-auth-master-firebase-adminsdk-lnv8u-c0ce8380be.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -20,20 +28,37 @@ const uri = "mongodb+srv://arabian:X3sgpUhoYyvrp52k@cluster0.muahx.mongodb.net/b
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const bookings = client.db("burjAlArab").collection("bookings");
-  app.post('/addBooking', (req,res)=>{
-      const newBooking = req.body;
-      bookings.insertOne(newBooking)
-      .then(result =>{
+  app.post('/addBooking', (req, res) => {
+    const newBooking = req.body;
+    bookings.insertOne(newBooking)
+      .then(result => {
         res.send(result.insertedCount > 0)
       })
-      console.log(newBooking);
+    console.log(newBooking);
   })
-  app.get('/bookings', (req,res)=>{
-    console.log(req.headers.authorization)
-    bookings.find({email: req.query.email})
-    .toArray((err, documents)=>{
-      res.send(documents)
-    })
+  app.get('/bookings', (req, res) => {
+    const bearer = req.headers.authorization;
+    if (bearer && bearer.startsWith('Bearer')) {
+      const idToken = bearer.split(' ')[1];
+      console.log(idToken);
+      admin.auth().verifyIdToken(idToken)
+        .then((decodedToken) => {
+          const tokenEmail = decodedToken.email;
+          const queryEmail = req.query.email;
+          console.log(tokenEmail, queryEmail)
+          if (tokenEmail == queryEmail) {
+            bookings.find({ email: req.query.email })
+              .toArray((err, documents) => {
+                res.send(documents)
+              })
+          }
+          console.log(uid)
+        })
+        .catch((error) => {
+          // Handle error
+        });
+    }
+
   })
 });
 
